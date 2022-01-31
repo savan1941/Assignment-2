e8ce015364cf34749a2ddbd8c14690ba867c3984
savan1941 <97402042+savan1941@users.noreply.github.com>
2022-01-31 23:11:23 +0530

    update

--- myScript.js
+++ myScript.js
@@ -1,10 +1,19 @@
+const dbtn = document.getElementById("del");
+const addtask = document.getElementById("addedtask");
+const checkbox = document.getElementById('check');
+addtask.addEventListener("click",onitemclick);
+document.addEventListener("DOMContentLoaded", getfromlocal);
 
+
+
+//add button clicked
 document.querySelector('#push').onclick = function(){
 
-    if(document.querySelector('#newtask input').
+if(document.querySelector('#newtask input').
     value.length == 0){
           alert("Please Enter a Task");
       }
+
       else{
           document.querySelector('#addedtask').innerHTML
           += `<div class="task" id="taskclass">
@@ -14,107 +23,186 @@ document.querySelector('#push').onclick = function(){
           ${document.querySelector('#newtask input').
           value}
           </span>
-          <button class="delete"><i class="material-icons">delete</i></button>
+          <button class="delete"  id="del">
+          <i class="material-icons">delete</i>
+          </button>
          </div>`;
+         
 
-        
-         var current_tasks = document.
-         querySelectorAll('.delete');
+         //deletetask
 
-         for(var i = 0; i < current_tasks.length; i++){
-             current_tasks[i].onclick = function(){
-                 this.parentElement.remove();
-             }
-         }
-         document.querySelector('#newtask input').value="";
+        //  const current_task = document.querySelectorAll(".delete");
+        //  for(var i = 0 ; i < current_task.length; i++){
+        //      current_task[i].onclick = function(){
+        //          this.parentElement.remove();
+        //          removelocalTodos(this.parentElement);
 
-        const addtask = document.getElementById("addedtask");
-        const checkbox = document.getElementById('check');
-        
-
-        checkbox.addEventListener("click",onitemclick);
-
-        function onitemclick(e){
-            const item = e.target;
-
-            if(item.classList[0] === "checkbox"){
-                console.log("sddd");
-                const todo = item.parentElement;
-                todo.classList.toggle('completed');
-            }
-        }
-
-        // function onitemclick(event){
-        //        console.log("savan");
-        //     const target = event.target;
-        //     const item = target.parentElement;
-           
-            
-        //         // item.classList.toggle('completed');
-        //     // item.style.display="none";
-              
-        //     if(target.classList[0]=="checkb"){
-        //         item.classList.add('completed');
-                
-        //     }
-        
-
-        // }
-
-        //  var tasks = document.
-        //  querySelectorAll('#taskname');  
-
-        //  for(var i = 0; i < tasks.length; i++){
-        //      tasks[i].onclick = function(){
-        //          this.classList.toggle('completed');
         //      }
         //  }
+       
+        //save in local storage
+        var todolst = document.getElementById("todoinput");
+        // let addtaskinputval = todolst.value;
+        saveltolocal(todolst.value);
         
+      
+        
+        
+        // let webtask = localStorage.getItem("elements");
 
-
-        //  const checkbox = document.getElementById('check');
-        //  var x = document.getElementsByClassName("checkb");
-        // //  const checkeded = document.getElementById('taskname');
-        //  const taskclass = document.getElementById('taskclass');
-        //  const desk = document.getElementById('deskbox');
-
-        //  for (var i = 0; i < x.length; i++) {
-        //     x[i].addEventListener('change', (event) => {
-        //     if (event.currentTarget.checked) {
-        //         console.log("sssss");
-                
-        //             taskclass.children[2].classList.add('completed');
-        //             desk.classList.remove('fa-minus-square');
-        //             desk.classList.add('fa-check-square');
-                   
-                
-        //     } else {
-        //         taskclass.children[2].classList.remove('completed');
-        //         desk.classList.remove('fa-check-square');
-        //         desk.classList.add('fa-minus-square');
-        //     }
-        //     })
+        // if(webtask == null){
+        //     elements = [];
         // }
 
-        //  for (var i = 0; i < x.length; i++) {
-             
-        //     x[i].addEventListener('change', function() {
-        //       if (this.checked) {
-        //           console.log("sssss")
-        //         // Checkbox is checked..
-        //         taskclass.children[2].classList.toggle('completed');
-        //       } else {
-        //         // Checkbox is not checked..
-        //         taskclass.children[2].classList.remove('completed');
-        //       }
-        //     })
-        //  }
-          
-       
+        // else{
+        //    elements = JSON.parse(webtask);
+        // }
+        // taskobj.push(addtaskinputval);
+        // localStorage.setItem("localtask",JSON.stringify(elements));
+
+        document.querySelector('#newtask input').value="";
+
+
     }
+
         
 
- }
+}
+    //    document.querySelector('#newtask input').value="";
+
+
+
+
+
+
+         // checkbox clicked and delete clicked
+        function onitemclick(e){
+            const item = e.target;
+            
+            
+            if(item.classList[0] == "material-icons"){
+               
+                
+                const parent = item.parentElement.parentElement;
+                removefromlocal(parent);
+                // console.log(item.parentElement);
+                parent.remove();
+            }
+            //for checkbox
+            if(item.classList[0] === "checkb"){
+                
+                console.log("sddd");
+                var todo = item.parentElement;
+                console.log(todo);
+
+
+
+              if(item.checked == true){
+                    console.log(item.nextElementSibling.nextElementSibling);
+                    item.nextElementSibling.nextElementSibling.classList.add("completed");
+                    item.nextElementSibling.classList.remove('fa-minus-square');
+                    item.nextElementSibling.classList.add('fa-check-square');
+                    // todo.classList.add('blur');
+
+                }
+              else{
+                    item.nextElementSibling.nextElementSibling.classList.remove("completed");
+                    item.nextElementSibling.classList.add('fa-minus-square');
+                    item.nextElementSibling.classList.remove('fa-check-square');
+                    // todo.classList.remove('blur');
+                }
+            }
+
+        }
+
+
+        // function saveltolocal(addtaskinputval){
+        // let webtask = localStorage.getItem("localtask");
+
+        // if(webtask == null){
+        //     taskobj = [];
+        // }
+
+        // else{
+        //    taskobj = JSON.parse(webtask);
+        // }
+        // taskobj.push(addtaskinputval);
+        // localStorage.setItem("localtask",JSON.stringify(taskobj));
+
+        // document.querySelector('#newtask input').value="";
+        // }
+
+
+
+    function removefromlocal(todo){
+        console.log(todo);
+        let todos;
+        let index;
+        if(localStorage.getItem("todos") === null){
+            todos = [];
+        }
+        else{
+            todos = JSON.parse(localStorage.getItem("todos"));
+        }
+        var child = todo.childNodes[1];
+        var textele = child.nextElementSibling.nextElementSibling.innerText;
+        console.log(textele);
+        
+        for(var i = 0; i<todos.length ; i++){
+            // console.log(todos[i]);
+            if(textele == todos[i]){ 
+                // console.log(index);
+                index = i;
+            }
+
+        }
+         
+        console.log(index);
+         todos.splice(index,1);
+        localStorage.setItem("todos",JSON.stringify(todos));
+    }
+
+    function saveltolocal(todo) {
+        // console.log(typeof(todo));
+        let elements;
+        if (localStorage.getItem("todos") === null) {
+          todos = [];
+        } else {
+          // getting the elements saved in localStorage in array
+          todos = JSON.parse(localStorage.getItem("todos"));
+        }
+      
+        
+        todos.push({"text": todo, "status": 0});
+      
+        localStorage.setItem("todos", JSON.stringify(todos));
+      }
+
+      function getfromlocal(){
+        let todos;
+        if (localStorage.getItem("todos") === null) {
+          todos = [];
+        } else {
+          // getting the elements saved in localStorage in array
+          todos = JSON.parse(localStorage.getItem("todos"));
+        }
+        todos.forEach((sd)=>{
+            // console.log(sd);
+            document.querySelector('#addedtask').innerHTML
+            += `<div class="task" id="taskclass">
+            <input type="checkbox" id="check" class="checkb">
+            <i class="fa fa-minus-square" id="deskbox" style="font-size:20px;"></i>
+            <span id="taskname">
+            ${sd.text} 
+            </span>
+            <button class="delete"  id="del">
+            <i class="material-icons">delete</i>
+            </button>
+           </div>`;
+
+        })
+
+      }
 
 
 
